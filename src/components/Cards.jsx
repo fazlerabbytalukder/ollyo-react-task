9e246a22896bf501bc4df7176be6f68e95242136
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2023-11-02 22:06:31 +0600

    checkbox stay when user check conditional randering tailwind class

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -39,12 +39,12 @@ const Cards = (props) => {
             onDrop={handleDrop}
         >
             <div className="insider-card relative group bg-white flex justify-center items-center border border-gray-400 rounded w-full h-auto">
-                <div className="absolute inset-0 bg-black opacity-0 group-hover:opacity-50 z-10"></div>
+                <div className={"absolute inset-0 bg-black opacity-0 group-hover:opacity-50 z-10"}></div>
                 <input
                     type="checkbox"
                     checked={props.isSelected}
                     onChange={handleCheckboxChange}
-                    className="absolute top-2 left-2 opacity-0 group-hover:opacity-100 z-20"
+                    className={`absolute top-2 left-2 opacity-0 group-hover:opacity-100 z-20 ${props.isSelected ? "opacity-100":"opacity-0"}`}
                 />
                 <img src={props.image} alt={`Image for ID: ${props.id}`} className="w-full object-cover object-center" />
             </div>
