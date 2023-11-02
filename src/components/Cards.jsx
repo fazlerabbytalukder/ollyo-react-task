06deb195ad2d0051a0266d417974b9b4d1ca8468
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2023-11-02 22:45:06 +0600

    add comment cards  comonent functionality to understand others

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,27 +3,35 @@ import React, { useState } from 'react';
 const Cards = (props) => {
     const [isDragging, setIsDragging] = useState(false);
 
+    // Function to handle checkbox state change.
     const handleCheckboxChange = () => {
         props.onCheckboxChange(props.id);
     };
 
+     // Function to handle the start of a drag operation.
     const handleDragStart = (e) => {
+        // Set the data being dragged and mark the card as dragging.
         e.dataTransfer.setData('text/plain', props.id);
         setIsDragging(true);
     };
 
+    // Function to handle the end of a drag operation.
     const handleDragEnd = () => {
+        // Mark the card as no longer dragging.
         setIsDragging(false);
     };
 
+    // Function to handle the drag-over event (prevents the default behavior).
     const handleDragOver = (e) => {
         e.preventDefault();
     };
 
+    // Function to handle the drop event and trigger card swapping.
     const handleDrop = (e) => {
         e.preventDefault();
         const draggedCardId = e.dataTransfer.getData('text/plain');
 
+        // Check if the dragged card is different from the current card and initiate a card swap.
         if (draggedCardId !== props.id) {
             props.onSwapCards(draggedCardId, props.id);
         }
